ce34a31a8e318e422d77ea42776f2a5f0936d767
Flibouche <kevin.pfiffer2@gmail.com>
2024-03-12 21:51:06 +0100

    Initialisation de mon exercice personnel sur le thème du RPG.

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>POO RPG</title>
+</head>
+<body>
+    <h1>Exercice personnel de POO</h1>
+    <h2>Thème : RPG</h2>
+<?php
+
+
+
+
+
+
+
+?>
+</body>
+</html>
